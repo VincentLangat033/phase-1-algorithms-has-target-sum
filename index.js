5c48cc729e3d5d49b84263cf59f70fc9a7836327
Vincent Langat <kimutailangat033@gmail.com>
2022-06-18 09:05:11 +0300

    commit tree 4ce0a3c3be834bab664f61dc0a421fbc80c18a29

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+    // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const resultingNmber = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === resultingNmber)
+       return true;
+    }
+  }
+
+  return false;
 }
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
+   function hasTargetSum(array, target)
+   Use a for loop
+   for(let i = 0, i<array.length, i ++ ){
+     resultingNumber = target - array[i]
+    //  now lets iterate over J
+    for (j=i+1, j<array.length, j ++){
+    if (array[j] === resultingNmber)
+    return true;
+    }
+    return false;
+
+   }
 */
 
 /* 
@@ -12,6 +36,13 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  create an object to keep track of numbers we've already seen
+  iterate through each number in the array
+    for the current number , we  identify a complement(resultingNumber) that adds to 10 
+    check if any key on our object is the complement(resultingNumber )
+      if so, return true
+      otherwise, add that number to the object
+  if I reach the end of the array, return false
 */
 
 // You can run `node index.js` to view these console logs
